5c6ceafbe3720ccf2c73afa9aee2320575954fcf
Jordy Alkema <6128820+jordycoding@users.noreply.github.com>
2023-03-12 15:46:59 +0100

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+import React, { useState } from "react";
+import { ChakraProvider } from "@chakra-ui/react";
+import "./App.css";
+
+function App() {
+  return <ChakraProvider></ChakraProvider>;
+}
+
+export default App;
